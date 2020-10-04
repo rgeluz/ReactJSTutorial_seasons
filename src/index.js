8acbe325999c5bd53ca7e82b17d81d0a63b5dc2c
Roman <rgeluz22@yahoo.com>
2020-10-03 23:31:27 -0700

    converted functional based component 'App' into class based comoponent

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-
+// functional based component
+/*
 const App = () => {
-  return <div>Hi there!</div>;
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
+  return <div>Latitude: </div>;
 };
+*/
+
+// class based component
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+
+
+
+    return <div>Latitude: </div>;
+  }
+}
+
 
 ReactDom.render(<App />, document.querySelector('#root'))
