6e83fc8ec5d13ed99de1839923b55d66d34759b7
Roman <rgeluz22@yahoo.com>
2020-10-11 14:33:52 -0700

    added icons to SeasonDipslay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr it is cold!",
+    iconName: "snowflake"
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -13,11 +24,16 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   //console.log(season);
   //return <div>Season Display</div>;
+
   const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
- 
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
